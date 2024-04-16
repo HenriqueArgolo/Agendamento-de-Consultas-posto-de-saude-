cced3c4e75e54362264fcdc3444f310eb8863ff5
HenriqueArgolo <henrique73357@gmail.com>
2024-04-16 12:20:39 -0300

    autentication

---
+++ app/src/main/java/com/ptn/postotancredo/view/atentication/Login.kt
@@ -0,0 +1,34 @@
+package com.ptn.postotancredo.view.atentication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.ptn.postotancredo.R
+import com.ptn.postotancredo.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setContentView(R.layout.activity_login)
+
+
+       navigationThroughtActivities()
+
+    }
+
+
+    private fun navigationThroughtActivities(){
+        binding.backButton.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
+    }
+
+    private fun login(){
+
+    }
+}
